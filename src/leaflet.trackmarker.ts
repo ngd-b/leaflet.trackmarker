a7e6899dba51d2f735c4bc872ee1f2b041a5c446
hboot <bobolity@163.com>
2025-08-15 11:18:43 +0800

    feat: 为 TrackMarker 添加详细文档注释
    - 在 TrackMarkerOptions 接口中添加了各个选项的文档注释
    - 在 TrackMarker 类中添加了各方法的文档注释
    - 优化了 trackMarker 函数的文档注释

--- src/leaflet.trackmarker.ts
+++ src/leaflet.trackmarker.ts
@@ -6,29 +6,91 @@ declare global {
   }
   namespace L {
     interface TrackMarkerOptions extends MarkerOptions {
+      /**
+       * 播放速度，默认 0.1 km/s
+       */
       speed?: number; // km/s
+      /**
+       * 是否自动播放，默认 true
+       */
       autoPlay?: boolean;
+      /**
+       * 是否旋转，默认 true
+       */
       rotation?: boolean;
+      /**
+       * 旋转偏移量，默认 0
+       * 单位：度
+       */
       rotationOffset?: number;
 
+      /**
+       * 播放完成回调
+       */
       onFinish?: () => void;
+      /**
+       * 播放进度回调
+       *
+       */
       onProgress?: () => void;
+      /**
+       * 暂停回调
+       *
+       */
       onPause?: () => void;
+      /**
+       * 播放开始回调
+       *
+       */
       onBeforePlay?: () => void;
+      /**
+       * 播放回调
+       *
+       */
       onPlay?: () => void;
+      /**
+       * 重置回调
+       *
+       */
       onReset?: () => void;
     }
 
     class TrackMarker extends L.Marker {
       constructor(line: L.Polyline, options?: TrackMarkerOptions);
+      /**
+       * 播放
+       */
       play(): this;
+      /**
+       * 暂停
+       */
       pause(): this;
+      /**
+       * 重置
+       *
+       */
       reset(): this;
+      /**
+       * 定位到指定百分比
+       *
+       * @param percent 百分比
+       */
       seek(percent: number): this;
+      /**
+       * 设置速度
+       *
+       * @param speed 速度
+       */
       setSpeed(speed: number): this;
     }
 
-    // 工厂函数类型
+    /**
+     * 轨迹移动
+     *
+     * @param line 轨迹线
+     * @param options 选项
+     * @returns this
+     */
     function trackMarker(
       line: L.Polyline,
       options?: TrackMarkerOptions
